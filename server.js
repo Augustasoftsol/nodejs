63e20ae419859c97e7d4477c5c6beef3dc31aae4
Ashok <nashok@augustasoftsol.com>
2015-07-15 09:39:51 -0400

    new

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = require('./app/config/express')();
 
 
 // Start the app by listening on <port>
-app.listen(8080);
+app.listen(3000);
 
 // Expose app
 exports = module.exports = app;
 
 // Logging initialization
-console.log('MEAN.JS application started on port ' + 8080);
+console.log('MEAN.JS application started on port ' + 3000);
